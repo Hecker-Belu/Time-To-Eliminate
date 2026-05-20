b54cee016fbd8a57642014ddd03aa312f87b6fc5
Rogue <140899536+Hecker-Belu@users.noreply.github.com>
2026-05-20 10:58:58 -0400

    commit

--- Time To Eliminate/Assets/Scripts/MainMenu.cs
+++ Time To Eliminate/Assets/Scripts/MainMenu.cs
@@ -18,4 +18,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Level 1");
     }
+
+    public void LevelSelect(string LevelName)
+    {
+        SceneManager.LoadScene(LevelName);
+    }
 }
